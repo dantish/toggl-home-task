041cbaace6aa897e62719ee9dcbddcb0d21c710b
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-02-11 22:36:30 +0200

    Introduce TimeEntry domain model

---
+++ ToggleHomeTask/Domain/TimeEntry.swift
@@ -0,0 +1,14 @@
+//
+//  TimeEntry.swift
+//  ToggleHomeTask
+//
+//  Created by Daniel Tischenko on 11.02.2023.
+//
+
+import Foundation
+
+struct TimeEntry {
+    let id: UUID
+    let startTime: Date
+    let endTime: Date
+}
