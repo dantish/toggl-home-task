876cc03453fc90a8689c5a9c186a20d1383b66e3
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-02-13 21:47:45 +0200

    `TimeTrackingPresenter.didLoadView` displays initial values

--- ToggleHomeTaskTests/Presentation/TimeTrackingPresenterTests.swift
+++ ToggleHomeTaskTests/Presentation/TimeTrackingPresenterTests.swift
@@ -6,9 +6,26 @@
 //
 
 import XCTest
+@testable import ToggleHomeTask
+
+protocol TimeTrackingView {
+    func display(_ viewModel: TimeTrackingViewModel)
+}
 
 final class TimeTrackingPresenter {
+    let view: TimeTrackingView
 
+    init(view: TimeTrackingView) {
+        self.view = view
+    }
+
+    func didLoadView() {
+        view.display(TimeTrackingViewModel(
+            stopwatchValue: "0",
+            toggleActionTitle: "Start",
+            isToggleActionDestructive: false
+        ))
+    }
 }
 
 final class TimeTrackingPresenterTests: XCTestCase {
@@ -19,20 +36,38 @@ final class TimeTrackingPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
 
+    func test_didLoadView_displaysInitialValues() {
+        let (sut, view) = makeSUT()
+
+        sut.didLoadView()
+
+        XCTAssertEqual(view.messages, [
+            .display(stopwatchValue: "0", toggleActionTitle: "Start", isToggleActionDestructive: false),
+        ])
+    }
+
     // MARK: - Helpers
 
     private func makeSUT() -> (sut: TimeTrackingPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = TimeTrackingPresenter()
+        let sut = TimeTrackingPresenter(view: view)
         return (sut, view)
     }
 
-    private class ViewSpy {
-        enum Message {
-
+    private class ViewSpy: TimeTrackingView {
+        enum Message: Equatable {
+            case display(stopwatchValue: String, toggleActionTitle: String, isToggleActionDestructive: Bool)
         }
-        
-        let messages: [Message] = []
+
+        private(set) var messages: [Message] = []
+
+        func display(_ viewModel: TimeTrackingViewModel) {
+            messages.append(.display(
+                stopwatchValue: viewModel.stopwatchValue,
+                toggleActionTitle: viewModel.toggleActionTitle,
+                isToggleActionDestructive: viewModel.isToggleActionDestructive
+            ))
+        }
     }
 
 }
