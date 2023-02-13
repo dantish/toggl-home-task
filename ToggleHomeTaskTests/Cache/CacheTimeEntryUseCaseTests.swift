b3929eca0f754cf3796f89903a23ab06f8abc04c
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-02-13 10:55:24 +0200

    `CacheTimeEntryUseCase.save` requests time entry insertion

--- ToggleHomeTaskTests/Cache/CacheTimeEntryUseCaseTests.swift
+++ ToggleHomeTaskTests/Cache/CacheTimeEntryUseCaseTests.swift
@@ -6,11 +6,37 @@
 //
 
 import XCTest
+@testable import ToggleHomeTask
 
-protocol TimeEntriesStore {}
+struct LocalTimeEntry: Equatable {
+    let id: UUID
+    let startTime: Date
+    let endTime: Date
+}
+
+protocol TimeEntriesStore {
+    typealias InsertionResult = Result<Void, Error>
+    typealias InsertionCompletion = (InsertionResult) -> Void
+
+    func insert(_ timeEntry: LocalTimeEntry, completion: @escaping InsertionCompletion)
+}
 
 final class CacheTimeEntryUseCase {
-    init(store: TimeEntriesStore) {}
+    let store: TimeEntriesStore
+
+    init(store: TimeEntriesStore) {
+        self.store = store
+    }
+
+    func save(_ timeEntry: TimeEntry) {
+        let local = LocalTimeEntry(
+            id: timeEntry.id,
+            startTime: timeEntry.startTime,
+            endTime: timeEntry.endTime
+        )
+
+        store.insert(local) { _ in }
+    }
 }
 
 final class CacheTimeEntryUseCaseTests: XCTestCase {
@@ -23,12 +49,41 @@ final class CacheTimeEntryUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [])
     }
 
+    func test_save_requestsTimeEntryInsertion() {
+        let store = TimeEntriesStoreSpy()
+        let sut = CacheTimeEntryUseCase(store: store)
+        let model = uniqueTimeEntry()
+        let local = LocalTimeEntry(
+            id: model.id,
+            startTime: model.startTime,
+            endTime: model.endTime
+        )
+
+        sut.save(model)
+
+        XCTAssertEqual(store.receivedMessages, [.insert(local)])
+    }
+
     // MARK: - Helpers
 
-    private final class TimeEntriesStoreSpy: TimeEntriesStore {
-        enum Message: Equatable {}
+    private func uniqueTimeEntry() -> TimeEntry {
+        TimeEntry(
+            id: UUID(),
+            startTime: Date(timeIntervalSinceNow: -10),
+            endTime: Date()
+        )
+    }
 
-        let receivedMessages: [Message] = []
+    private final class TimeEntriesStoreSpy: TimeEntriesStore {
+        enum Message: Equatable {
+            case insert(LocalTimeEntry)
+        }
+
+        private(set) var receivedMessages: [Message] = []
+
+        func insert(_ timeEntry: LocalTimeEntry, completion: @escaping InsertionCompletion) {
+            receivedMessages.append(.insert(timeEntry))
+        }
     }
 
 }
