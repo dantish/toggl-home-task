caba836078361f5a09b185420698050443c5f2ec
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2023-02-13 21:15:04 +0200

    Implement the Time Tracking UI with `TimeTrackingViewController`

---
+++ ToggleHomeTask/UI/TimeTrackingViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TimeTrackingViewController.swift
+//  ToggleHomeTask
+//
+//  Created by Daniel Tischenko on 13.02.2023.
+//
+
+import UIKit
+
+struct TimeTrackingViewModel {
+    let stopwatchValue: String
+    let toggleActionTitle: String
+    let isToggleActionDestructive: Bool
+}
+
+final class TimeTrackingViewController: UIViewController {
+    @IBOutlet weak var stopwatchLabel: UILabel!
+    @IBOutlet weak var toggleButton: UIButton!
+
+    var onViewLoaded: (() -> Void)?
+    var onStopwatchToggled: (() -> Void)?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        stopwatchLabel.font = .boldSystemFont(ofSize: 72)
+
+        toggleButton.configuration = .filled()
+        toggleButton.configuration?.cornerStyle = .capsule
+
+        onViewLoaded?()
+    }
+
+    func display(_ viewModel: TimeTrackingViewModel) {
+        stopwatchLabel.text = viewModel.stopwatchValue
+
+        toggleButton.configuration?.attributedTitle = AttributedString(viewModel.toggleActionTitle, attributes: AttributeContainer([
+            .font: UIFont.boldSystemFont(ofSize: 24)
+        ]))
+
+        toggleButton.configuration?.baseBackgroundColor = viewModel.isToggleActionDestructive
+            ? .systemRed
+            : .systemBlue
+    }
+
+    @IBAction func switchButtonTapped(_ sender: Any) {
+        onStopwatchToggled?()
+    }
+}
